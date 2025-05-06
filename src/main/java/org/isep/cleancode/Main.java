67f371b7c272e1687e959e7c1a63435f08feea09
Ganettte <morgane2002.lebon@gmail.com>
2025-05-06 21:21:32 +0200

    V4

--- src/main/java/org/isep/cleancode/Main.java
+++ src/main/java/org/isep/cleancode/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
-        String expression = "12 + 3 * 2 - 5";
+        String expression = "12 + 3 * 2 - 5.5";
         double restulat = calculator.evaluerExrepessionMath(expression);
 
         System.out.println("Expression : " + expression);
